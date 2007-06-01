619671da25b0c08d68b6fd39b4ddbb3adce4fda9
brianegge <brianegge@95f2dd7e-b031-0410-832e-11057ee6a2da>
2007-06-01 09:34:31 +0000

    Start work on gimmeh

--- test/com/lolcode/Count10Test.java
+++ test/com/lolcode/Count10Test.java
@@ -1,7 +1,5 @@
 package com.lolcode;
 
-import junit.framework.TestCase;
-
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
@@ -9,15 +7,10 @@ import java.util.List;
 /**
  * @author brianegge
  */
-public class Count10Test extends TestCase {
-
-    private final TestRuntime runtime = new TestRuntime();
+public class Count10Test extends RuntimeTestCase {
 
     public void testCount10() throws FileNotFoundException, ParseException {
-        LolCode parser = new LolCode(new java.io.FileInputStream("test/samples/Count!!1.LOL"));
-        LolTree tree = parser.start();
-        Run run = new Run(tree, runtime);
-        run.run();
+        parse("Count!!1.LOL");
         List<String> s = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
         assertEquals(s, Arrays.asList(runtime.getStdOut().split(Runtime.EOL)));
         assertEquals("", runtime.getStdErr());
---
+++ test/com/lolcode/GimmehTestCase.java
@@ -0,0 +1,16 @@
+package com.lolcode;
+
+import java.io.FileNotFoundException;
+
+/**
+ * @author brianegge
+ */
+public class GimmehTestCase extends RuntimeTestCase {
+
+    public void test() throws FileNotFoundException, ParseException {
+//        runtime.setStdIn("ROTFL" + Runtime.EOL);
+//        parse("gimmeh.LOL");
+//        assertEquals("You said ROTFL !!" + Runtime.EOL, runtime.getStdOut());
+//        assertEquals("", runtime.getStdErr());
+    }
+}
